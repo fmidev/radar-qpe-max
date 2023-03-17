ec0009cbe87c9bb2e1b4345d498404fafa643d3a
Jussi Tiira <jussi.tiira@fmi.fi>
2023-03-17 18:11:27 +0200

    rolling max to geotiff proof of concept

--- src/scripts/maxit_dev.py
+++ src/scripts/maxit_dev.py
@@ -1,5 +1,6 @@
 import os
 
+import xarray as xr
 import rioxarray
 import pyart
 import numpy as np
@@ -23,13 +24,7 @@ def select_center(da, size=2048):
     return da[:, ystart:ystart+size, xstart:xstart+size].copy()
 
 
-if __name__ == '__main__':
-    plt.close('all')
-    datadir = os.path.expanduser('~/data/alakulma')
-    fpath = os.path.join(datadir, '202206030010_radar.raw.fmi_SITE=fivih_TASKS=PPI3-A.h5')
-    radar = pyart.aux_io.read_odim_h5(fpath)
-    z_r_qpe(radar)
-    axr = plot_ppi(radar, sweep=0, what=lwe)
+def precip_grid(radar):
     gf = basic_gatefilter(radar)
     grid_shape = (1, 4100, 4100)
     r_m = 320000.0
@@ -42,5 +37,23 @@ if __name__ == '__main__':
     rda.rio.write_crs(4326, inplace=True)
     rda3067 = rda.rio.reproject("epsg:3067", resolution=250, nodata=np.nan)
     rda3067 = select_center(rda3067, size=2048)
-    fig, ax = plt.subplots()
-    rda3067[0].plot(ax=ax)
+    return rda3067
+
+
+if __name__ == '__main__':
+    plt.close('all')
+    datadir = os.path.expanduser('~/data/alakulma')
+    fpath0 = os.path.join(datadir, '202206030010_radar.raw.fmi_SITE=fivih_TASKS=PPI3-A.h5')
+    fpath1 = os.path.join(datadir, '202206030015_radar.raw.fmi_SITE=fivih_TASKS=PPI1-A.h5')
+    fpath2 = os.path.join(datadir, '202206030020_radar.raw.fmi_SITE=fivih_TASKS=PPI2-A.h5')
+    rdas = []
+    for fpath in [fpath0, fpath1, fpath2]:
+        radar = pyart.aux_io.read_odim_h5(fpath)
+        z_r_qpe(radar)
+        axr = plot_ppi(radar, sweep=0, what=lwe)
+        rdas.append(precip_grid(radar))
+    rda = xr.concat(rdas, 'time').fillna(0)
+    max2 = rda.rolling({'time': 2}).max()[1:]
+    for dat in max2:
+        tstamp = dat.time.item().strftime('%Y%m%d%H%M')
+        dat.rio.to_raster(f"/tmp/{tstamp}_max.tif")
