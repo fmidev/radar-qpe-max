6a701194de21850f1adc0cc295ba4c1d1b40da70
Jussi Tiira <jussi.tiira@fmi.fi>
2023-03-20 16:35:10 +0200

    vrk demo

--- src/scripts/maxit_dev.py
+++ src/scripts/maxit_dev.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 
 import xarray as xr
 import rioxarray
@@ -24,36 +25,41 @@ def select_center(da, size=2048):
     return da[:, ystart:ystart+size, xstart:xstart+size].copy()
 
 
-def precip_grid(radar):
+def precip_grid(radar, size=2048, resolution=250):
     gf = basic_gatefilter(radar)
-    grid_shape = (1, 4100, 4100)
-    r_m = 320000.0
+    intermsize = int(size*2)
+    grid_shape = (1, intermsize, intermsize)
+    r_m = size*resolution*0.625 # includes reprojection margin
     grid_limits = ((0, 5000), (-r_m, r_m), (-r_m, r_m))
     grid = pyart.map.grid_from_radars(radar, gatefilters=gf,
                                       grid_shape=grid_shape,
-                                      grid_limits=grid_limits)
-    rds = grid.to_xarray().isel(z=0).set_index(x='lon', y='lat').reset_coords()
+                                      grid_limits=grid_limits,
+                                      fields=[lwe])
+    rds = grid.to_xarray().isel(z=0).set_index(x='lon', y='lat').reset_coords(drop=True)
     rda = rds.lwe_precipitation_rate
     rda.rio.write_crs(4326, inplace=True)
-    rda3067 = rda.rio.reproject("epsg:3067", resolution=250, nodata=np.nan)
-    rda3067 = select_center(rda3067, size=2048)
-    return rda3067
+    rda3067 = rda.rio.reproject("epsg:3067", resolution=resolution, nodata=np.nan)
+    rda3067 = select_center(rda3067, size=size)
+    return rda3067#.reset_coords(drop=True)
 
 
 if __name__ == '__main__':
     plt.close('all')
+    win = 12
     datadir = os.path.expanduser('~/data/alakulma')
-    fpath0 = os.path.join(datadir, '202206030010_radar.raw.fmi_SITE=fivih_TASKS=PPI3-A.h5')
-    fpath1 = os.path.join(datadir, '202206030015_radar.raw.fmi_SITE=fivih_TASKS=PPI1-A.h5')
-    fpath2 = os.path.join(datadir, '202206030020_radar.raw.fmi_SITE=fivih_TASKS=PPI2-A.h5')
+    #fpath0 = os.path.join(datadir, '202206030010_radar.raw.fmi_SITE=fivih_TASKS=PPI3-A.h5')
+    #fpath1 = os.path.join(datadir, '202206030015_radar.raw.fmi_SITE=fivih_TASKS=PPI1-A.h5')
+    #fpath2 = os.path.join(datadir, '202206030020_radar.raw.fmi_SITE=fivih_TASKS=PPI2-A.h5')
+    fpaths = sorted(glob(os.path.join(datadir, '*-A.h5')))
     rdas = []
-    for fpath in [fpath0, fpath1, fpath2]:
+    for fpath in fpaths: #[fpath0, fpath1, fpath2]:
         radar = pyart.aux_io.read_odim_h5(fpath)
         z_r_qpe(radar)
-        axr = plot_ppi(radar, sweep=0, what=lwe)
-        rdas.append(precip_grid(radar))
+        #axr = plot_ppi(radar, sweep=0, what=lwe)
+        rdas.append(precip_grid(radar, size=512, resolution=1000))
     rda = xr.concat(rdas, 'time').fillna(0)
-    max2 = rda.rolling({'time': 2}).max()[1:]
-    for dat in max2:
-        tstamp = dat.time.item().strftime('%Y%m%d%H%M')
-        dat.rio.to_raster(f"/tmp/{tstamp}_max.tif")
+    del(rdas)
+    accums = rda.rolling({'time': win}).sum()[win-1:]/12
+    dat = accums.max('time')
+    tstamp = accums.time[-1].item().strftime('%Y%m%d')
+    dat.rio.to_raster(f"/tmp/{tstamp}_max.tif")
