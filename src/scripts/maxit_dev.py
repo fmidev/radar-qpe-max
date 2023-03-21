04417abbb9817121b3f6841f4bd5c25ce928cef1
Jussi Tiira <jussi.tiira@fmi.fi>
2023-03-21 19:18:45 +0200

    tiff uint16 encoding

--- src/scripts/maxit_dev.py
+++ src/scripts/maxit_dev.py
@@ -4,12 +4,23 @@ from glob import glob
 import xarray as xr
 import rioxarray
 import pyart
+from pyart.graph.common import generate_radar_time_begin
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from radproc.aliases import zh, lwe
 from radproc.visual import plot_ppi
 from radproc.radar import z_r_qpe
+
+
+LWE_SCALE_FACTOR = 0.01
+LWE_FILL_VALUE = np.iinfo(np.uint16).max
+DEFAULT_ENCODING = {lwe: {'zlib': True, 'dtype': 'u2', 'scale_factor': LWE_SCALE_FACTOR}}
+ATTRS = {lwe: {'units': 'mm h-1',
+               '_FillValue': LWE_FILL_VALUE}}
+CACHE_DIR = os.path.expanduser('~/.cache/sademaksit')
+os.makedirs(CACHE_DIR, exist_ok=True)
 
 
 def basic_gatefilter(radar, field=zh):
@@ -25,7 +36,7 @@ def select_center(da, size=2048):
     return da[:, ystart:ystart+size, xstart:xstart+size].copy()
 
 
-def precip_grid(radar, size=2048, resolution=250):
+def precip_grid(radar, outfile, size=2048, resolution=250):
     gf = basic_gatefilter(radar)
     intermsize = int(size*2)
     grid_shape = (1, intermsize, intermsize)
@@ -38,28 +49,38 @@ def precip_grid(radar, size=2048, resolution=250):
     rds = grid.to_xarray().isel(z=0).set_index(x='lon', y='lat').reset_coords(drop=True)
     rda = rds.lwe_precipitation_rate
     rda.rio.write_crs(4326, inplace=True)
-    rda3067 = rda.rio.reproject("epsg:3067", resolution=resolution, nodata=np.nan)
+    rda3067 = rda.rio.reproject("epsg:3067", resolution=resolution, nodata=LWE_FILL_VALUE)
     rda3067 = select_center(rda3067, size=size)
-    return rda3067#.reset_coords(drop=True)
+    rda3067.to_dataset().to_netcdf(outfile, encoding=DEFAULT_ENCODING)
 
 
 if __name__ == '__main__':
     plt.close('all')
     win = 12
+    chunksize = 128
+    ignore_cache = False
+    chunks = {'x': chunksize, 'y': chunksize}
     datadir = os.path.expanduser('~/data/alakulma')
-    #fpath0 = os.path.join(datadir, '202206030010_radar.raw.fmi_SITE=fivih_TASKS=PPI3-A.h5')
-    #fpath1 = os.path.join(datadir, '202206030015_radar.raw.fmi_SITE=fivih_TASKS=PPI1-A.h5')
-    #fpath2 = os.path.join(datadir, '202206030020_radar.raw.fmi_SITE=fivih_TASKS=PPI2-A.h5')
-    fpaths = sorted(glob(os.path.join(datadir, '*-A.h5')))
-    rdas = []
-    for fpath in fpaths: #[fpath0, fpath1, fpath2]:
+    resultsdir = os.path.expanduser('~/results/sademaksit')
+    h5paths = sorted(glob(os.path.join(datadir, '*-A.h5')))
+    for fpath in h5paths:
         radar = pyart.aux_io.read_odim_h5(fpath)
+        t = generate_radar_time_begin(radar)
+        ts = t.strftime('%Y%m%d%H%M')
+        outfile = os.path.join(CACHE_DIR, f'tstep{ts}.nc')
+        if os.path.isfile(outfile) and not ignore_cache:
+            continue
         z_r_qpe(radar)
-        #axr = plot_ppi(radar, sweep=0, what=lwe)
-        rdas.append(precip_grid(radar, size=512, resolution=1000))
-    rda = xr.concat(rdas, 'time').fillna(0)
-    del(rdas)
-    accums = rda.rolling({'time': win}).sum()[win-1:]/12
+        precip_grid(radar, outfile, size=512, resolution=1000)
+    ncglob = os.path.join(CACHE_DIR, 'tstep*.nc')
+    rds = xr.open_mfdataset(ncglob, chunks=chunks, data_vars='minimal', engine='rasterio')
+    accums = (rds[lwe].rolling({'time': win}).sum()[win-1:]/12).to_dataset()
     dat = accums.max('time')
+    dat['time'] = accums[lwe].idxmax('time')
     tstamp = accums.time[-1].item().strftime('%Y%m%d')
-    dat.rio.to_raster(f"/tmp/{tstamp}_max.tif")
+    cachenc = os.path.join(CACHE_DIR, f'{tstamp}.nc')
+    dat[lwe].attrs = ATTRS[lwe]
+    dat.to_netcdf(cachenc, encoding=DEFAULT_ENCODING)
+    datrio = rioxarray.open_rasterio(cachenc).rio.write_crs(3067)
+    tif = os.path.join(resultsdir, f'{tstamp}_max1h.tif')
+    datrio[lwe].rio.to_raster(tif, dtype='uint16')
