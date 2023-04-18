5ffec2bae9b367a55d09bbd2985ff408198597b9
Jussi Tiira <jussi.tiira@fmi.fi>
2023-04-18 12:35:33 +0300

    use proj in pyart gridding

--- src/scripts/maxit_dev.py
+++ src/scripts/maxit_dev.py
@@ -15,7 +15,7 @@ from radproc.aliases import zh, lwe
 from radproc.radar import z_r_qpe
 
 
-PYART_AEQD_FMT='+proj=aeqd +lon_0={lon} +lat_0={lat} +R=6370997'
+PYART_AEQD_FMT='+proj={proj} +lon_0={lon_0} +lat_0={lat_0} +R={R}'
 LWE_SCALE_FACTOR = 0.01
 UINT16_FILLVAL = np.iinfo(np.uint16).max
 DEFAULT_ENCODING = {lwe: {'zlib': True, 'dtype': 'u2', 'scale_factor': LWE_SCALE_FACTOR}}
@@ -47,7 +47,7 @@ def select_center(da, size=2048):
 def pyart_aeqd(radar):
     lat = radar.latitude['data'][0]
     lon = radar.longitude['data'][0]
-    return PYART_AEQD_FMT.format(lat=lat, lon=lon)
+    return dict(proj='aeqd', lat_0=lat, lon_0=lon, R=6370997)
 
 
 def save_precip_grid(radar, outfile, size=2048, resolution=250):
@@ -74,7 +74,8 @@ def save_precip_grid2(radar, outfile, size=2048, resolution=250):
     grid_shape = (1, size, size)
     grid_limits = ((0, 5000), (-r_m, r_m), (-r_m, r_m))
     grid = pyart.map.grid_from_radars(radar, gatefilters=gf, grid_shape=grid_shape,
-                                      grid_limits=grid_limits, fields=[lwe])
+                                      grid_limits=grid_limits, fields=[lwe],
+                                      grid_projection=pyart_aeqd(radar))
     rds = grid.to_xarray().isel(z=0).reset_coords(drop=True)
     transproj = Transformer.from_crs(pyart_aeqd(radar), 'EPSG:3067')
     x, y = transproj.transform(rds.x, rds.y)
@@ -110,7 +111,7 @@ def maxit(h5paths, resultsdir, size=2048, resolution=250, win='1D',
     rollsel = rds.sel(time=slice(tstep_pre, tstep_last))
     accums = (rollsel[lwe].rolling({'time': iwin}).sum()/12).to_dataset()
     dat = accums.max('time').rio.write_crs(3067)
-    dat['time'] = accums[lwe].idxmax('time')
+    dat['time'] = accums[lwe].idxmax(dim='time', keep_attrs=True)
     tstamp = accums.time[-1].dt.strftime('%Y%m%d').item()
     dat[lwe].attrs.update(ATTRS[lwe])
     dat['time'].attrs.update(ATTRS['time'])
@@ -128,9 +129,10 @@ def maxit(h5paths, resultsdir, size=2048, resolution=250, win='1D',
 
 if __name__ == '__main__':
     plt.close('all')
+    clear_cache()
     date = datetime.date(2022, 6, 4)
     #
     resultsdir = os.path.expanduser('~/results/sademaksit')
     datadir = os.path.expanduser('~/data/alakulma')
     h5paths = sorted(glob(os.path.join(datadir, '*-A.h5')))
-    maxit(h5paths, resultsdir, size=512, resolution=1000)
+    maxit(h5paths, resultsdir, size=512, resolution=1000, ignore_cache=True)
