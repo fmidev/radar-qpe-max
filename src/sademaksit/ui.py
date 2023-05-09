d7fb0ef8e920ca6800032580163ba8a45b2ec18a
Jussi Tiira <jussi.tiira@fmi.fi>
2023-05-09 14:55:49 +0300

    first cli works

--- src/sademaksit/ui.py
+++ src/sademaksit/ui.py
@@ -3,10 +3,25 @@ import datetime
 
 import click
 
-from sademaksit import DATEFMT
+from sademaksit import DATEFMT, ls_low_elev, maxit
 
 
 @click.command()
-@click.option('-t', '--date', help='yyyymmdd')
-def cli(date):
-    t = datetime.datetime.strptime(date, DATEFMT)
+@click.argument('yyyymmdd')
+@click.option('-i', '--input-dir', metavar='DIR', help='directory containing hdf5 input data')
+@click.option('-o', '--output-dir', metavar='DIR', help='write output here')
+@click.option('-s', '--size', metavar='PX', help='output raster size PX*PX', default=1024)
+@click.option('-r', '--resolution', metavar='METRE', help='spatial resolution in meters')
+def cli(yyyymmdd, input_dir, output_dir, size, resolution):
+    if resolution is None:
+        if size>1999:
+            resolution = 250
+        elif size>999:
+            resolution = 500
+        elif size>499:
+            resolution = 1000
+        else:
+            resolution = 2000
+    date = datetime.datetime.strptime(yyyymmdd, DATEFMT)
+    h5paths = ls_low_elev(date, input_dir)
+    maxit(date, h5paths, output_dir, size=size, resolution=resolution)
