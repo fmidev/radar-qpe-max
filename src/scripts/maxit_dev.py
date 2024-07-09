8a7ca6b8ac7883b5e8e15c9e9f30ac2e6e398214
Jussi Tiira <jussi.tiira@fmi.fi>
2024-07-09 13:50:58 +0300

    SCR: use different cache dir for debugging

--- src/scripts/maxit_dev.py
+++ src/scripts/maxit_dev.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     #datadir = os.path.expanduser('~/data/polar/fivih')
     datadir = os.path.expanduser('~/data/polar/filuo')
     h5paths = two_day_glob(date, globfmt=os.path.join(datadir, '{date}*.h5'))
-    # size 1024 can still be run on a laptop with chunksize 256
-    maxit(date, h5paths, resultsdir, size=1024, resolution=500, chunksize=256,
-          ignore_cache=False, debug=True)
+    cachedir = os.path.expanduser('~/.cache/sademaksit')
+    # size 2048 can still be run on a laptop with chunksize 256
+    maxit(date, h5paths, resultsdir, size=512, resolution=1000, chunksize=256,
+          ignore_cache=False, cache_dir=cachedir, debug=True)
