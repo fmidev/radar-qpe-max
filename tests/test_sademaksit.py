5330543d4f515266078659fe9c775d58c98c9d89
Jussi Tiira <jussi.tiira@fmi.fi>
2024-07-16 13:54:14 +0300

    test step_from_fpaths

--- tests/test_sademaksit.py
+++ tests/test_sademaksit.py
@@ -1,7 +1,9 @@
+import datetime
+
 import pyart
 import numpy as np
 
-from maksitiirain import basic_gatefilter, ZH
+from maksitiirain import basic_gatefilter, ZH, tstep_from_fpaths
 
 
 def test_basic_gatefilter():
@@ -12,3 +14,29 @@ def test_basic_gatefilter():
     assert isinstance(gatefilter, pyart.filters.GateFilter)
     assert np.all(gatefilter.gate_excluded == 0)
     assert np.all(gatefilter.gate_included == 1)
+
+
+def test_tstep_from_fpaths():
+    fpaths = [
+        "/path/to/202405280000_radar.polar.filuo.h5",
+        "/path/to/202405280005_radar.polar.filuo.h5",
+        "/path/to/202405280010_radar.polar.filuo.h5",
+    ]
+    expected_tstep = datetime.timedelta(minutes=5)
+    assert tstep_from_fpaths(fpaths) == expected_tstep
+
+    fpaths = [
+        "/path/to/202405280000_radar.polar.filuo.h5",
+        "/path/to/202405280010_radar.polar.filuo.h5",
+        "/path/to/202405280020_radar.polar.filuo.h5",
+    ]
+    expected_tstep = datetime.timedelta(minutes=10)
+    assert tstep_from_fpaths(fpaths) == expected_tstep
+
+    fpaths = [
+        "/path/to/202405280000_radar.polar.filuo.h5",
+        "/path/to/202405280015_radar.polar.filuo.h5",
+        "/path/to/202405280030_radar.polar.filuo.h5",
+    ]
+    expected_tstep = datetime.timedelta(minutes=15)
+    assert tstep_from_fpaths(fpaths) == expected_tstep
