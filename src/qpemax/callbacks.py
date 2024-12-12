feff03e2ca7f98e05c5d9d4e88b5e9b400ac0662
Jussi Tiira <jussi.tiira@fmi.fi>
2024-12-12 16:14:44 +0200

    tracker process

--- src/qpemax/callbacks.py
+++ src/qpemax/callbacks.py
@@ -3,6 +3,47 @@ import threading
 from timeit import default_timer
 from dask.callbacks import Callback
 from dask.utils import format_time
+import psutil
+from multiprocessing import Pipe, Process, current_process
+
+
+class _Tracker(Process):
+    """Background process to track memory usage
+    of the children of the current process"""
+
+    def __init__(self, dt=1):
+        super().__init__()
+        self.dt = dt
+        self.parent_pid = current_process().pid
+        self.parent_conn, self.child_conn = Pipe()
+
+    def shutdown(self):
+        if not self.parent_conn.closed:
+            self.parent_conn.send("shutdown")
+            self.parent_conn.close()
+        self.join()
+
+    def _update_pids(self, pid):
+        return [self.parent] + [
+            p for p in self.parent.children() if p.pid != pid and p.status() != "zombie"
+        ]
+
+    def run(self):
+        self.parent = psutil.Process(self.parent_pid)
+        pid = current_process()
+        while True:
+            try:
+                msg = self.child_conn.recv()
+            except KeyboardInterrupt:
+                continue
+            if msg == "shutdown":
+                break
+            pids = self._update_pids(pid)
+            memory = sum(p.memory_info().rss / 1e6 for p in pids)
+            self.child_conn.send(memory)
+            time.sleep(self.dt)
+        self.child_conn.close()
+
 
 class ProgressLogging(Callback):
     def __init__(self, logger, dt=1):
